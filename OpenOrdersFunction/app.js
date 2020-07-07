1fd198a5c38223bf98d5b2f45fa1aa4b2650e236
James Oliver <joliver.robert@me.com>
2020-07-07 11:42:56 +0100

    updates table_number from event parameters to event body

--- OpenOrdersFunction/app.js
+++ OpenOrdersFunction/app.js
@@ -13,52 +13,50 @@ exports.handler = async (event, context) => {
   // eslint-disable-next-line no-console
   console.log(context, 'this is the context');
 
-  const { venue_id, table_number } = event.queryStringParameters;
+  const { venue_id } = JSON.parse(event.body);
   const { connectionId } = event.requestContext;
 
-  if (table_number === 'dashboard') {
-    let openOrders;
+  let openOrders;
 
-    try {
-      const scanParams = {
-        TableName: TABLE_ORDERS,
-        FilterExpression:
-          '#order_status = :order_accepted AND #order_status = :order_pending AND #venue_id = :venue_id',
-        ExpressionAttributeNames: {
-          '#order_status': 'order_status',
-          '#venue_id': 'venue_id'
-        },
-        ExpressionAttributeValues: {
-          ':order_accepted': 'completed',
-          ':order_pending': 'pending',
-          ':venue_id': venue_id
-        }
-      };
+  try {
+    const scanParams = {
+      TableName: TABLE_ORDERS,
+      FilterExpression:
+        '#order_status = :order_accepted AND #order_status = :order_pending AND #venue_id = :venue_id',
+      ExpressionAttributeNames: {
+        '#order_status': 'order_status',
+        '#venue_id': 'venue_id'
+      },
+      ExpressionAttributeValues: {
+        ':order_accepted': 'completed',
+        ':order_pending': 'pending',
+        ':venue_id': venue_id
+      }
+    };
 
-      openOrders = await ddb.scan(scanParams).promise();
-      // eslint-disable-next-line no-console
-      console.log(openOrders, 'Open orders');
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error, 'Error reading open orders');
-    }
+    openOrders = await ddb.scan(scanParams).promise();
+    // eslint-disable-next-line no-console
+    console.log(openOrders, 'Open orders');
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error, 'Error reading open orders');
+  }
 
-    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-      apiVersion: '2018-11-29',
-      endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`
-    });
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`
+  });
 
-    const postData = JSON.stringify(openOrders.Items);
+  const postData = JSON.stringify(openOrders.Items);
 
-    try {
-      await apigwManagementApi
-        .postToConnection({ ConnectionId: connectionId, Data: postData })
-        .promise();
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error, 'error sending message');
-      return { statusCode: 500, body: error.stack };
-    }
+  try {
+    await apigwManagementApi
+      .postToConnection({ ConnectionId: connectionId, Data: postData })
+      .promise();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error, 'error sending message');
+    return { statusCode: 500, body: error.stack };
   }
 
   return { statusCode: 200, body: 'Connected.' };
